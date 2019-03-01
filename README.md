ed8184c69beadc31cc46f7cf37d492c646afac0d
Jijeesh <jijeesh.ka@gmail.com>
2019-03-01 15:44:40 +0530

    update with output file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # terraform-ansible-aws-jenkins
+
+##Generating a new SSH key
+```
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+```
+
+## Usage
+```
+cd app-test
+./run.sh
+```
+
+## Cleanup
+
+```
+cd app-test
+terraform destroy
+```
--- app-test/run.sh
+++ app-test/run.sh
@@ -4,19 +4,21 @@
 terraform_run () {
   echo "================Starting terraform================"
 
-  terraform init && terraform plan  && terraform apply
+  terraform init
+  # terraform plan
+  terraform apply -auto-approve
   echo "================Terraforming Complete============="
 }
 
-ansible_run(){
-  echo "==================Starting Ansible================"
-  ansible-playbook -s main.yml
-  echo "================Ansible Complete============="
-}
+# ansible_run(){
+#   echo "==================Starting Ansible================"
+#   ansible-playbook -s main.yml
+#   echo "================Ansible Complete============="
+# }
 
 main () {
   terraform_run
-  ansible_run  
+  # ansible_run
 }
 
 main
